7f2763c22822dfa465186b59667a1304cc000a90
Dtanguy <demme.tanguy@gmail.com>
2022-01-06 16:42:26 +0100

    mobile topBar

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -10,6 +10,23 @@
         <TwitterLogo class="svg" />
       </div>
     </div>
+
+    <div class="right">
+      <a class="icon" v-on:click="hide = !hide">
+        <i class="fa fa-bars"></i>
+      </a>
+    </div>
+
+    <div class="myLinks" v-if="hide">
+      <a class="nav-item2" @click="goTo('Shop')">
+        <i class="fa fa-instagram fa-so"></i>
+        <p class="text">Instagram</p></a
+      >
+      <a class="nav-item2" @click="goTo('D3PrintExample')">
+        <i class="fa fa-twitter fa-so" />
+        <p class="text">Twitter</p></a
+      >
+    </div>
   </div>
 </template>
 
@@ -24,7 +41,18 @@ export default {
     msg: String,
   },
   components: { GlitchTxt, DiscordLogo, TwitterLogo },
+  data() {
+    return {
+      hide: false,
+    };
+  },
+  mounted() {
+    window.addEventListener("resize", this.onresize);
+  },
   methods: {
+    onresize() {
+      this.hide = false;
+    },
     goToExternal(url) {
       window.open(url);
     },
@@ -33,6 +61,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.right {
+  display: none;
+}
+
+.myLinks {
+  display: none;
+}
+
 .topBar {
   position: relative;
   top: 25px;
@@ -90,25 +126,29 @@ export default {
 @media screen and (max-width: $layout-breakpoint-medium) {
   .topBar {
     position: fixed;
-
+    padding: 0%;
+    margin: 0;
     top: 0px;
-    height: 80px;
+    height: 65px;
     width: calc(100% - 50px);
     padding: 0 25px;
     box-shadow: 0px 4px 7px -5px #ffffff;
-    background-color: $background-color1;
+    background-color: rgba(#00000b, 0.98);
   }
 
   .title2 {
     font-size: 14px;
     margin-top: 5px;
+    width: 300px;
   }
 
   .socialsContainer {
+    display: none;
     margin-right: -20px;
   }
 
   .logoContainer {
+    display: none;
     width: 40px;
     min-width: 40px;
     height: 40px;
@@ -119,5 +159,65 @@ export default {
   .svg {
     zoom: 75%;
   }
+
+  .right {
+    display: block;
+    margin-right: -25px;
+  }
+}
+
+.myLinks {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  position: absolute;
+  top: 65px;
+  left: 0px;
+  border-bottom: 1px solid #c2c2c2;
+  width: 100%;
+}
+
+.nav-item2 {
+  align-items: center;
+  white-space: nowrap;
+  user-select: none;
+  padding: 20px 20px;
+  box-shadow: 0px 0px 7px 0px #ffffff;
+  background-color: rgba(#00000b, 0.98);
+  color: $white-color;
+  text-decoration: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.icon {
+  color: $white-color;
+  font-size: 30px;
+  margin: 0;
+  padding: 15px 20px;
+  padding-right: 22px;
+  &:hover {
+    background: rgba(#c2c2c2, 0.3);
+  }
+}
+
+.fa-so {
+  color: $white-color;
+  font-size: 30px;
+  opacity: 0.85;
+  &:hover {
+    box-shadow: 0 7px 14px rgba(50, 50, 93, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08);
+    opacity: 1;
+    transform: translateY(-1px);
+  }
+}
+
+.text {
+  font-family: "MADE Sunflower", sans-serif;
+  text-transform: uppercase;
+  font-weight: 100;
+  letter-spacing: 0.3em;
+  font-size: 13px;
 }
 </style>
