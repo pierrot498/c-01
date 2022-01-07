66ec47be0c12d4abcbb78b4c37b74f004ee03f7c
Dtanguy <demme.tanguy@gmail.com>
2022-01-07 15:46:26 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -224,23 +224,33 @@ export default {
       },
       Faq: [
         {
-          q: "Quo illo voluptatem et soluta error est error recusandae ?",
-          r: "Quo illo voluptatem et soluta error est error recusandae ?",
+          q: "What is the mint price ?",
+          r: "The mint price is 0.25 ETH plus gas",
           hide: true,
         },
         {
-          q: "Quo illo voluptatem et soluta error est error recusandae ?",
-          r: "Quo illo voluptatem et soluta error est error recusandae ?",
+          q: "How many NFT can I mint per wallet ? ",
+          r: "You can mint a maximum of 2 NTFs per Ethereum wallet.",
           hide: true,
         },
         {
-          q: "Quo illo voluptatem et soluta error est error recusandae ?",
-          r: "Quo illo voluptatem et soluta error est error recusandae ?",
+          q: "How can I register for the raffle ?",
+          r: "Visit our website on Tuesday 13th at 2pm UTC to register for the raffle. Please be aware that you need enough funds on your wallet to mint at least 1 C-01 NFT. We will only verify the wallet addresses with a minimum of 0.28 ETH (0.25 + Gas fees).",
           hide: true,
         },
         {
-          q: "Quo illo voluptatem et soluta error est error recusandae ?",
-          r: "Quo illo voluptatem et soluta error est error recusandae ?",
+          q: "When is the public sale ?",
+          r: "The public sale will occur on January 14th, at 4 pm UTC on our website. Only the winners of the raffle will be able to mint.",
+          hide: true,
+        },
+        {
+          q: "How do I know if I won the raffle?",
+          r: "Please visit our website on Friday,14th at 4pm UTC and connect your Metamask account to see if you've won. If you win the raffle, you will be able to mint right away.",
+          hide: true,
+        },
+        {
+          q: "Do I need to register for the raffle if I’m whitelisted?",
+          r: "If you are whitelisted, you will be able to mint 2 hours prior to the public sale on Friday,14th at 2pm UTC. You will be guaranteed to mint up to 2 NFTs. You can however register for the raffle with a different Ethereum address.",
           hide: true,
         },
       ],
