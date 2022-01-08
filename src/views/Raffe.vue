8f3ec307683b10495d16f3c7aaa2fa32c216116d
Dtanguy <demme.tanguy@gmail.com>
2022-01-08 17:12:05 +0100

    Update Raffe.vue

--- src/views/Raffe.vue
+++ src/views/Raffe.vue
@@ -2,7 +2,23 @@
   <div class="raffe">
     <div class="viewContainer">
       <div class="center">
-        <GlitchTxt class="title1 title3 t4">Raffe /</GlitchTxt>
+        <GlitchTxt class="title1 title3 t4">Raffle /</GlitchTxt>
+
+        <div class="raffeContainer" @mouseenter="mouseEnter" @mousemove="mouseMove" @mouseleave="mouseLeave">
+          <div class="inline3">
+            <div class="cornerLeftTop2" />
+            <div class="cornerRightTop2" />
+          </div>
+          <div class="texte txt">
+            <p class="title1">Register to the raffle now</p>
+            <p>If you are whitelisted, you will be able to mint 2 hours prior to the public sale on Friday,14th at 2pm UTC. You will be guaranteed to mint up to 2 NFTs. You can however register for the raffle with a different Ethereum address.</p>
+          </div>
+          <div class="inline3">
+            <div class="cornerLeftBottom2" />
+            <div class="cornerRightBottom2" />
+          </div>
+        </div>
+        <button class="regButton">Register</button>
       </div>
     </div>
   </div>
@@ -21,4 +37,101 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.raffeContainer {
+  margin-top: 100px;
+  > * {
+    transition: all 500ms ease-in-out;
+  }
+  &:hover .txt {
+    padding: 50px;
+    cursor: url("../assets/cursors/Cursor2Glow60px.png") 32 32, auto;
+  }
+}
+
+.inline3 {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.txt {
+  font-family: "Helvetica Neue", sans-serif;
+  max-width: 700px;
+  min-width: 500px;
+  font-size: 20px;
+  margin: auto;
+  //margin: -40px 20px;
+
+  > * {
+    transition: all 500ms ease-in-out;
+  }
+}
+
+.title1 {
+  font-size: 25px;
+}
+
+.cornerLeftTop2 {
+  width: 60px;
+  height: 60px;
+  border-radius: 8px 0 0 0;
+  box-sizing: border-box;
+  filter: drop-shadow(0px 0px 4px $white-color);
+  border-left: 2px solid $white-color;
+  border-top: 2px solid $white-color;
+}
+
+.cornerRightTop2 {
+  width: 60px;
+  height: 60px;
+  border-radius: 0 8px 0 0;
+  box-sizing: border-box;
+  filter: drop-shadow(0px 0px 4px $white-color);
+  border-right: 2px solid $white-color;
+  border-top: 2px solid $white-color;
+}
+
+.cornerLeftBottom2 {
+  width: 60px;
+  height: 60px;
+  border-radius: 0 0 0 8px;
+  box-sizing: border-box;
+  filter: drop-shadow(0px 0px 4px $white-color);
+  border-left: 2px solid $white-color;
+  border-bottom: 2px solid $white-color;
+  // margin-top: -5px;
+  // margin-right: calc(100% - 62px);
+}
+
+.cornerRightBottom2 {
+  width: 60px;
+  height: 60px;
+  border-radius: 0 0 8px 0;
+  box-sizing: border-box;
+  filter: drop-shadow(0px 0px 4px $white-color);
+  border-right: 2px solid $white-color;
+  border-bottom: 2px solid $white-color;
+  //margin-top: -5px;
+  // margin-left: calc(100% - 62px);
+}
+
+.regButton {
+  font-family: "MADE Sunflower", sans-serif;
+  margin: auto;
+  margin-top: 50px;
+  width: 300px;
+  height: 60px;
+  border-radius: 5px;
+  //filter: drop-shadow(0px 0px 4px $white-color);
+  border: 1px solid $white-color;
+  background-color: transparent;
+  color: $white-color;
+  font-size: 25px;
+  font-weight: bold;
+  text-transform: uppercase;
+  text-align: center;
+  line-height: 50px;
+  cursor: url("../assets/cursors/Cursor2Glow60px.png") 32 32, auto;
+}
+</style>
