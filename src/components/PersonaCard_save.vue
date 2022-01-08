399dd84a4555316a0ef64c0b93399ab11eac6120
Dtanguy <demme.tanguy@gmail.com>
2022-01-08 17:15:59 +0100

    up

--- src/components/PersonaCard_save.vue
+++ src/components/PersonaCard_save.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="teamMember">
     <p class="text name">{{ name }}</p>
-    <div class="personaContainer">
+    <div class="personaContainer" @mouseenter="mouseEnter" @mousemove="mouseMove" @mouseleave="mouseLeave">
+      <div class="cornerLeftTop2" />
       <img class="persona" :style="style" :src="pic" />
+      <div class="cornerRightBottom2" />
     </div>
     <div class="socialsContainer2">
       <div class="logoContainer2" v-if="insta != ''" @click="goToExternal(insta)">
@@ -43,6 +45,26 @@ export default {
   },
   mounted() {},
   methods: {
+    mouseEnter() {
+      //console.log("mouseEnter");
+    },
+    mouseLeave() {
+      //console.log("mouseLeave");
+      this.style = "transform: none";
+    },
+    mouseMove(e) {
+      let rect = e.target.getBoundingClientRect();
+      let dx = e.clientX - rect.top / 2;
+      let dy = e.clientY - rect.left / 2;
+      //console.log(dx, dy);
+
+      let tiltx = dy / rect.top / 1.5;
+      let tilty = -(dx / rect.left) / 1.5;
+      let radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
+      let degree = radius * 20;
+
+      this.style = "transform: rotate3d(" + tiltx + ", " + tilty + ", 0, " + degree + "deg)";
+    },
     goToExternal(url) {
       window.open(url);
     },
@@ -52,12 +74,21 @@ export default {
 
 <style lang="scss" scoped>
 .teamMember {
-  margin: 50px;
+  width: 350px;
+  margin: 10px;
   transition: all 500ms ease-in-out;
   opacity: 0.85;
 
   > * {
     transition: all 500ms ease-in-out;
+  }
+
+  &:hover {
+    opacity: 1;
+    margin: 0;
+  }
+  &:hover .persona {
+    margin: -30px 30px;
   }
 }
 
@@ -65,7 +96,7 @@ export default {
   text-transform: uppercase;
   font-size: 12px;
   margin: auto;
-  margin-bottom: 40px;
+  margin-bottom: -10px;
   width: 200px;
   // white-space: nowrap;
   // overflow: hidden;
@@ -80,10 +111,10 @@ export default {
 
 .persona {
   width: 300px;
-  height: 300px;
-  border-radius: 10000px;
+  height: 400px;
+  border-radius: 10px;
   object-fit: cover;
-  // margin: -40px 20px;
+  margin: -40px 20px;
   cursor: url("../assets/cursors/Cursor2Glow60px.png") 32 32, auto;
 }
 
@@ -96,6 +127,17 @@ export default {
   border-left: 2px solid $white-color;
   border-top: 2px solid $white-color;
 }
+.cornerRightBottom2 {
+  width: 60px;
+  height: 60px;
+  border-radius: 0 0 8px 0;
+  box-sizing: border-box;
+  filter: drop-shadow(0px 0px 4px $white-color);
+  border-right: 2px solid $white-color;
+  border-bottom: 2px solid $white-color;
+  margin-top: -5px;
+  margin-left: calc(100% - 62px);
+}
 
 .socialsContainer2 {
   display: flex;
@@ -103,7 +145,8 @@ export default {
   justify-content: flex-start;
   align-items: center;
   width: 100%;
-  margin-top: -10px;
+  margin-left: 15px;
+  margin-top: -25px;
 }
 
 .logoContainer2 {
